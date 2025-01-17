3861c293ff278d807813f1fc2ee30ee398a9ddae
ParkGMo <167054605+ParkGMo@users.noreply.github.com>
2025-01-17 20:49:53 +0900

    1.17.20:49

--- BIOINFO_project/BIOINFO_sql.sql
+++ BIOINFO_project/BIOINFO_sql.sql
@@ -1,5 +1,6 @@
 CREATE DATABASE IF NOT EXISTS 'SNP_matrix';
-CREATE DATABASE "SNP_matrix"
-CREATE TABLE "SNP_matrix_results" 
-CREATE TABLE "SNP_matrix_results_reverse"
-
+CREATE DATABASE "SNP_matrix";
+CREATE TABLE IF NOT EXISTS 'SNP_matrix_results';
+CREATE TABLE "SNP_matrix_results" ('SNP' VARCHAR(100), 'SAMPLE1' VARCHAR(2), 'SAMPLE2' VARCHAR(2));
+CREATE TABLE IF NOT EXISTS 'SNP_matrix_results_reverse';
+CREATE TABLE "SNP_matrix_results_reverse" ('SAMPLE' VARCHAR(100), 'SNP1' VARCHAR(2), 'SNP2' VARCHAR(2));
