feb6197811388633705a19c7d4ffa80fc785bda3
ParkGMo <167054605+ParkGMo@users.noreply.github.com>
2025-04-01 22:36:21 +0900

    4.1.22:36

--- FitStack/mysql/user.sql
+++ FitStack/mysql/user.sql
@@ -2,20 +2,6 @@ USE fittrack;
 
 DROP TABLE IF EXISTS fit_user;
 
--- CREATE TABLE fit_user (
---     user_id INT AUTO_INCREMENT PRIMARY KEY,
---     username VARCHAR(50) UNIQUE NOT NULL,
---     email VARCHAR(100) UNIQUE NOT NULL,
---     password_hash VARCHAR(255) NOT NULL,
---     age INT,
---     gender ENUM('Male', 'Female', 'Other'),
---     height_cm DECIMAL(5,2),
---     weight_kg DECIMAL(5,2),
---     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
---     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
---     deleted_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
--- );
-
 CREATE TABLE fit_user (
     user_id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) UNIQUE NOT NULL,
